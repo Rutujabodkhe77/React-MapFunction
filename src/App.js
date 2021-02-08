9b95242d0d6822d61459a24c5de057c42099b458
Rutujabodkhe77 <rutubodkhe77@gmail.com>
2021-02-08 09:44:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App()
+{
+    // const allowedState=[
+    //     {MyCar:"BMW"},
+    //     {MyCar:"TESALA"},
+    //     {MyCar:"TATA"}
+        
+    // ]
+
+return(
+    <div>
+        <h1>Select Tag</h1>
+        <h1>React Map Function</h1>
+         
+{/* <table>
+    <tr>
+        <td>Name</td>
+    </tr>
+</table> */}
     </div>
-  );
-}
+)
 
+};
 export default App;
