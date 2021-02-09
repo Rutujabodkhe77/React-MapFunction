5a545c4e9552429739e491c711784906c535db1d
Rutujabodkhe77 <rutubodkhe77@gmail.com>
2021-02-09 07:59:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
+
+import  './MyCss.css';
+import Rutuja from './Rutuja.js';
+
 
 function App()
 {
-    // const allowedState=[
-    //     {MyCar:"BMW"},
-    //     {MyCar:"TESALA"},
-    //     {MyCar:"TATA"}
-        
-    // ]
 
-return(
-    <div>
-        <h1>Select Tag</h1>
-        <h1>React Map Function</h1>
-         
-{/* <table>
-    <tr>
-        <td>Name</td>
-    </tr>
-</table> */}
+  // cons is constant you can't change this
+  const allowedState = 
+  [
+    { MyCar: "BMW", Owner : "ABC" },
+    { MyCar: "TESLA", Owner : "ELON " },
+    { MyCar: "TATA", Owner : "RATAN TATA " },
+   
+  ];
+  return(
+    <div className="rutuja">
+       <h1><b>....This is MApFunction....</b></h1>
+      <h1>Select Tag (Dropdown)</h1>
+      <select>
+              {allowedState.map((i, index) => (
+                <option key={i.id}>{i.MyCar}</option>
+              ))}
+      </select>
+     
+      <h1>  ol Tag (ordered List)</h1>
+      <ol>
+              {allowedState.map((i, index) => (
+                <li key={i.id}>{i.MyCar}</li>
+              ))}
+      </ol>
+
+      <h1>ul Tag (unordered List)</h1>
+      <ul>
+              {allowedState.map((i, index) => (
+                <li >{i.MyCar}</li>
+              ))}
+      </ul>
+
+      <h1>Table</h1>
+      <table className="MyTable">
+                <tr>
+
+                    <td>Sr No</td> 
+                    <td>Name</td>
+                    <td>Owner Name</td>
+                </tr>
+                
+            {allowedState.map((i, index) => (
+                  <tr>
+                <td key={i.id}>{index+1}</td>
+                <td key={i.id}>{i.MyCar}</td>
+                <td key={i.id}>{i.Owner}</td>
+                </tr>
+              ))}
+      </table>
+      <Rutuja />
     </div>
-)
+  );
+}
 
-};
 export default App;
---
+++ src/Rutuja.js
@@ -0,0 +1,11 @@
+function Rutuja()
+{
+    return(
+        <div>
+            <p>Paragraph tag</p>
+            <p>Hello This is Rutuja Component </p>
+        </div>
+    );
+}
+
+export default Rutuja;
